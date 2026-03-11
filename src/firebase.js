93840de58ad4cd0d1b683b2c245a1084027cb3a1
bukumzy <kumzyd@gmail.com>
2026-03-11 08:22:21 +0100

    Updated UI commit 2

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, onValue, update } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCi8Ig3JuFSr-E0jM9BYIq-uNajWW5MTKg",
+  authDomain: "math-competition-a9885.firebaseapp.com",
+  databaseURL: "https://math-competition-a9885-default-rtdb.firebaseio.com",
+  projectId: "math-competition-a9885",
+  storageBucket: "math-competition-a9885.firebasestorage.app",
+  messagingSenderId: "579384934096",
+  appId: "1:579384934096:web:f4a09b8bd4eaa6950268d5",
 };
 
 const app = initializeApp(firebaseConfig);
